3820afb48f0350abcfff805a7bda76b4bb30be61
hannache2277 <hannache2277@gmail.com>
2023-03-17 07:47:49 +0300

    cheange mile homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int ticetPrice = 150;
+        int oneBonusMile = 20;
+
+        int bonusMile = ticetPrice / oneBonusMile;
+
+        System.out.println("Итоговое количество бонусных миль = " + bonusMile);
     }
 }
